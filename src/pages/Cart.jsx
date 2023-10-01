a4de867f5b7213076b6b9a0ae451f94252d543ad
Yuvraj Jha <yuvraj.jha2001@gmail.com>
2023-10-02 01:55:55 +0530

    CartItem created

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
+import CartItem from '../components/CartItem';
 
 const Cart = () => {
   
-  const [cart] = useSelector((store)=> store);
+  const {cart} = useSelector((store)=> store);
 
 
   return (
-    
+    <div>
+      {
+        cart.map(item=>{
+          <CartItem />
+        })
+      }
+    </div>
   );
 }
 
